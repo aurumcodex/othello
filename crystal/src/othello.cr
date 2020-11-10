b00b21f29c4d487d48021b15501ddbe1959dc353
aurumcodex <aurumcodex@protonmail.com>
2020-11-10 12:47:27 -0500

    add crystal implementation

---
+++ crystal/src/othello.cr
@@ -0,0 +1,127 @@
+# TODO: Write documentation for `Othello`
+require "colorize"
+
+require "./othello/*"
+
+module Othello
+  extend self
+  VERSION = "0.1.0"
+
+  COLORIZE = true # will make this a local variable later
+
+  def main
+    turn_count = 0
+    human = true
+    debug = false
+
+    puts "what color do you want to play as? (black or white)"
+    input = gets.not_nil!.chomp
+
+    game = Board.new
+    puts "typeof game: #{typeof (game)}"
+    while true
+      puts "got \"#{input}\" from you."
+      case input
+      when "b", "B", "black", "Black", "BLACK"
+        game.setup(Color::Black)
+        break
+      when "w", "W", "white", "White", "WHITE"
+        game.setup(Color::White)
+        break
+      else
+        puts "unable to get valid input; please re-enter"
+        input = gets.not_nil!.chomp
+      end
+    end # end input validation loop
+
+    # puts game.player.color
+    # puts game.bot.color
+    # player = game.player
+    # bot = game.bot
+
+    current_player = game.player.color
+
+    while !game.game_over
+      movelist = Array(Move).new
+      cells = Array(Int32).new
+
+      puts "turn count is: #{turn_count}"
+
+      if current_player.black?
+        movelist.clear
+        cells.clear
+        movelist = game.generate_moves(game.player.color)
+
+        game.display(movelist, COLORIZE)
+
+        puts "legal moves:"
+        movelist.each do |m|
+          m.display(game.player.color)
+          cells << m.cell
+        end
+
+        if movelist.size == 0
+          puts "player has to pass"
+          game.player.get_pass_input(game.bot)
+        else
+          m = game.player.get_input(cells, human)
+          game.apply(game.player.color, m, debug)
+          movelist.each do |mv|
+            if mv.cell == m
+              game.flip_discs(game.player.color, mv.direction.invert, mv.cell, debug)
+            end
+          end
+        end
+      elsif current_player.white?
+        movelist.clear
+        cells.clear
+        movelist = game.generate_moves(game.bot.color)
+
+        game.display(movelist, COLORIZE)
+
+        puts "legal moves:"
+        movelist.each do |m|
+          m.display(game.bot.color)
+          cells << m.cell
+        end
+
+        if movelist.size == 0
+          puts "bot has to pass"
+          if game.player.passing == false
+            game.player.passing = true
+          else
+            game.bot.passing = true
+          end
+        else
+          m = game.bot.make_move(movelist, game, debug)
+          if !cells.includes? m
+            puts "bot made an odd move; using rng fallback"
+            m = game.rng_move(movelist)
+          end
+
+          puts "bot generated move: #{Util.shorten(game.bot.color)} #{Util.get_col(m)} #{Util.get_row(m)}"
+          game.apply(game.bot.color, m, debug)
+
+          movelist.each do |mv|
+            if mv.cell == m
+              game.flip_discs(game.bot.color, mv.direction.invert, mv.cell, debug)
+            end
+          end
+        end
+      end # end branches for player colors
+
+      current_player = current_player.invert
+      game.game_over = game.is_game_over?
+
+      turn_count += 1
+    end # end main game while loop
+
+    puts "game has ended | turns taken: #{turn_count}"
+
+    # scores = game.calculate_scores_disc
+    # print_results(scores)
+  end # end def main
+end   # end module Othello
+
+# call main game loop here
+Othello.main
---
+++ crystal/src/othello/move.cr
@@ -0,0 +1,157 @@
+# move.cr
+class Move
+  property cell : Int32
+  property num_flips : Int32
+  property direction : Direction
+
+  def initialize
+    @cell = -1
+    @num_flips = 0
+    @direction = Direction::Null
+  end
+
+  def is_border?
+    if Util::LEFT_BORDER.includes? self.cell
+      if self.direction == Direction::West.invert ||
+         self.direction == Direction::NWest.invert ||
+         self.direction == Direction::SWest.invert
+        return true
+      end
+    elsif Util::RIGHT_BORDER.includes? self.cell
+      if self.direction == Direction::East.invert ||
+         self.direction == Direction::NEast.invert ||
+         self.direction == Direction::SEast.invert
+        return true
+      end
+    else
+      return false
+    end
+  end
+
+  def weight
+    Util::WEIGHTS[self.cell]
+  end
+
+  def check_border
+    if Util::LEFT_BORDER.includes? self.cell
+      if self.direction == Direction::West.invert ||
+         self.direction == Direction::NWest.invert ||
+         self.direction == Direction::SWest.invert
+        return true
+      end
+    elsif Util::RIGHT_BORDER.includes? self.cell
+      if self.direction == Direction::East.invert ||
+         self.direction == Direction::NEast.invert ||
+         self.direction == Direction::SEast.invert
+        return true
+      end
+    else
+      return false
+    end
+  end
+
+  def display(color)
+    # self.format(stdout)
+    print "#{Util.shorten(color)} #{Util.get_col(@cell)} #{Util.get_row(@cell)} (#{@cell})| "
+    print "num flips: #{@num_flips} | "
+    puts "direction: #{@direction}"
+  end
+end
+
+module Movelist
+  # this gets included in Board, thus making the `self` work properly
+
+  def get_legal_move(index, dir, color) : Move
+    flips = 0
+    m = Move.new
+    wall = false
+
+    while index >= 0 && index < Util::BOARD_SIZE && !wall
+      wall = Moves.check_wall(index, dir)
+      index += dir.value
+
+      if index >= 0 && index < Util::BOARD_SIZE
+        if self.field[index] != color.invert
+          break
+        else
+          flips += 1
+        end
+      else
+        flips = 0
+        break
+      end
+    end
+
+    if index >= 0 &&
+       index < Util::BOARD_SIZE &&
+       self.field[index] == Color::None &&
+       flips != 0
+      m.cell = index
+      m.num_flips = flips
+      m.direction = dir
+    end
+
+    return m
+  end
+
+  def generate_moves(color) : Array(Move)
+    result = Array(Move).new
+
+    self.field.each_with_index do |val, index|
+      if val == color
+        Util::DIRECTIONS.each do |d|
+          m = self.get_legal_move(index, d, color)
+
+          if m.num_flips != 0 && !m.check_border
+            result << m
+          end
+        end
+      end
+    end
+
+    return result
+  end
+end
+
+module Moves
+  extend self # important to extend self
+
+  def check_wall(cell, direction)
+    case direction
+    when Direction::East
+      if Util::RIGHT_BORDER.includes? cell
+        return true
+      end
+    when Direction::West
+      if Util::LEFT_BORDER.includes? cell
+        return true
+      end
+    when Direction::NEast
+      if Util::RIGHT_BORDER.includes? cell || Util::TOP_BORDER.includes? cell
+        return true
+      end
+    when Direction::NWest
+      if Util::LEFT_BORDER.includes? cell || Util::BOTTOM_BORDER.includes? cell
+        return true
+      end
+    when Direction::SEast
+      if Util::RIGHT_BORDER.includes? cell || Util::TOP_BORDER.includes? cell
+        return true
+      end
+    when Direction::SWest
+      if Util::LEFT_BORDER.includes? cell || Util::BOTTOM_BORDER.includes? cell
+        return true
+      end
+    else
+      return false
+    end
+  end
+
+  def get_cells(moveset)
+    result = Array(Int32).new
+    moveset.each do |mv|
+      result << mv.cell
+    end
+    return result
+  end
+end
